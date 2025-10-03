8b24d2dd865ba89db2b633eb59edb273604a54e3
Ahmed Hajjami <A.hajjami.95@gmail.com>
2025-10-03 12:37:33 +0000

    fix pos page

--- src/components/pos/pos-client.tsx
+++ src/components/pos/pos-client.tsx
@@ -211,7 +211,7 @@ export function PosClient({
         </ScrollArea>
       </div>
 
-      <Card className="md:col-span-1 h-full flex flex-col">
+      <Card className="md:col-span-1 h-screen flex flex-col">
         <CardHeader>
           <CardTitle className="flex items-center justify-between gap-3">
             <div className="flex items-center gap-3">
