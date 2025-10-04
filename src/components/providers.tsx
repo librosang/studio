d2c5bcc4c27a9c31c1a3b81dc6d1f4c046b9cdd4
Ahmed Hajjami <A.hajjami.95@gmail.com>
2025-10-04 11:00:57 +0000

    Try fixing this error: `Runtime Error: Error: Element type is invalid: e

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -1,3 +1,4 @@
+
 'use client';
 
 import { I18nProvider } from '@/locales/client';
--- src/locales/client.ts
+++ src/locales/client.ts
@@ -1,9 +1,14 @@
+'use client';
 
-'use client'
+import { createI18nClient } from 'next-international/client';
 
-import { createI18nClient } from 'next-international/client'
-
-export const { useI18n, useScopedI18n, I18nProvider, useChangeLocale, useCurrentLocale } = createI18nClient({
+const I18nClient = createI18nClient({
   en: () => import('./en'),
   ar: () => import('./ar'),
-})
+});
+
+export const I18nProvider = I18nClient.I18nProvider;
+export const useI18n = I18nClient.useI18n;
+export const useScopedI18n = I18nClient.useScopedI18n;
+export const useChangeLocale = I18nClient.useChangeLocale;
+export const useCurrentLocale = I18nClient.useCurrentLocale;
