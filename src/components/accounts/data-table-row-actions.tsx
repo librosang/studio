1a329fdf1e18d9b0761ad74cf69708b847b25da7
Ahmed Hajjami <A.hajjami.95@gmail.com>
2025-10-04 10:07:34 +0000

    Uncaught Error: AlertDialogTrigger is not defined , please check again i

--- src/components/accounts/data-table-row-actions.tsx
+++ src/components/accounts/data-table-row-actions.tsx
@@ -25,6 +25,7 @@ import {
   AlertDialogFooter,
   AlertDialogHeader,
   AlertDialogTitle,
+  AlertDialogTrigger,
 } from '@/components/ui/alert-dialog';
 import { Icons } from '../icons';
 import { UserProfile } from '@/lib/types';
